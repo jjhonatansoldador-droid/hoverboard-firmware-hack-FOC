f0c4927bdb702db82bfb940f8c76bf04fa35c648
jjhonatansoldador-droid <jjhonatansoldador@gmail.com>
2026-06-01 18:42:52 -0300

    Agora deu tudo certo#16

--- Inc/config.h
+++ Inc/config.h
@@ -95,8 +95,8 @@
 #define CTRL_MOD_REQ VLT_MODE // ← TORQUE mode (alterado)
 #define DIAG_ENA 1
 
-#define I_MOT_MAX 20 // ← alterado de 15 para 20
-#define I_DC_MAX 17 // ← alterado de 17 para 22
+#define I_MOT_MAX 25 // ← alterado de 15 para 20
+#define I_DC_MAX 27 // ← alterado de 17 para 22
 #define N_MOT_MAX 800 // ← alterado de 1000 para 800
 
 #define FIELD_WEAK_ENA 0
